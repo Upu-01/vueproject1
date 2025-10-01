8e9149f93c455c2355d34e104918fa991d6bfe1b
Upu-01 <alisamaltseva2007@gmail.com>
2025-10-01 14:08:16 +0400

    добавлен файл navigation.vue в папке ui(shared)

--- vueproject1/src/App.vue
+++ vueproject1/src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <BApp teleport-to="body">
+    <BContainer>
+      <Navigation></Navigation>
     <router-view></router-view>
+    </BContainer>
   </BApp>
 </template>
 
 <style scoped></style>
 <script setup lang="ts">
-import {BApp, BCard, BButton} from 'bootstrap-vue-next'
+import {BApp, BCard, BButton, BContainer} from 'bootstrap-vue-next'
+import Navigation from './shared/ui/navigation.vue';
 </script>
---
+++ vueproject1/src/shared/ui/navigation.vue
@@ -0,0 +1,20 @@
+<template>
+        <BNav class="navigation w-100 p-3" >
+            <BLink to="/Admin" class="link">Админка</BLink>
+        </BNav>
+</template>
+
+<script setup lang="ts">
+import { BLink } from 'bootstrap-vue-next'
+</script>
+<style scoped>
+.navigation{
+    background-color: rgb(203, 255, 238);
+
+}
+
+.link{
+    text-decoration: none;
+    color: black;
+}
+</style>
